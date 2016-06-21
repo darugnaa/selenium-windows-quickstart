aa6a586d864d8f81f155e7eaa93503c4f10e3db4
Alessandro Da Rugna <alessandro.darugna@gmail.com>
2016-06-21 19:26:51 +0200

    Firefox and Edge tests

--- SeleniumWindowsPython/testselenium/testedge.py
+++ SeleniumWindowsPython/testselenium/testedge.py
@@ -4,16 +4,19 @@ from selenium import webdriver
 
 log = logging.getLogger(__name__)
 
+EDGE_DRIVER_PATH = 'C:/Program Files (x86)/Microsoft Web Driver/MicrosoftWebDriver.exe'
 
-class FirefoxTestCase(unittest.TestCase):
-    
+
+class ChromeTestCase(unittest.TestCase):
+
     def setUp(self):
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Edge(executable_path=EDGE_DRIVER_PATH)
         self.driver.implicitly_wait(5)
         self.driver.get('http://seleniumhq.org/')
 
     def test_browseSite(self):
-        log.info('--- FIREFOX %s ---', self.driver.capabilities.get('version'))
+        vers = self.driver.capabilities.get('version')
+        log.info('--- EDGE %s ---', str(vers))
         news = self.driver.find_elements_by_xpath("//div[@id='mainContent']/ul/li")
         log.info('We have a total of %s news', len(news))
         for n in news:
